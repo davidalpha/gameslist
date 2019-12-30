11a364efa78a29fce436a53c71204368a37c6ce1
David <david@Davids-MacBook-Air.local>
2019-12-30 16:44:43 +0100

    first

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <games />
+  </div>
+</template>
+
+<script>
+import games from './components/games.vue'
+
+export default {
+  name: 'app',
+  components: {
+    games
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/games.vue
@@ -0,0 +1,30 @@
+<template>
+  <h3>Games:</h3>
+</template>
+
+<script>
+  import axios from 'axios';
+
+  export default {
+    name: 'games',
+    data() {
+      return {
+        games: null
+    };
+  },
+
+  created: function() {
+    axios
+      .get('https://sheets.googleapis.com/v4/spreadsheets/1KlxXzRMWO3MdSEoQANpTuJOfFZ9iOiYtoPCxN9qc6CM/values/Sheet1?key=AIzaSyAOYzRf8SZJi4y7kaqYP8zSQ9orD1rJLyA')
+      .then(res => {
+        this.games = res.data;
+      })
+  }
+}
+</script>
+
+<style>
+  h3 {
+    margin-bottom: 5%;
+  }
+</style>
